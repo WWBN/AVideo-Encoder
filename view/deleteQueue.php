386afda91e4d3e987266da734cee4df20f0e1ae4
daniel <daniel@daniel-DC1A-S>
2017-08-05 14:23:08 -0300

    Only admin can delete somebody else queue

--- view/deleteQueue.php
+++ view/deleteQueue.php
@@ -3,12 +3,20 @@
 header('Content-Type: application/json');
 require_once dirname(__FILE__) . '/../videos/configuration.php';
 require_once '../objects/Encoder.php';
+require_once '../objects/Login.php';
 
 $obj = new stdClass();
 $obj->error = true;
 if (!empty($_POST['id'])) {
     $obj->item = $_POST['id'];
     $e = new Encoder($_POST['id']);
+    if(!Login::isAdmin()){
+        if(Login::getStreamerId() != $e->getStreamers_id()){
+            $obj->msg = "You must be Admin to be able to delete somebody else queue";
+            echo json_encode($obj);
+            exit;
+        }
+    }
     if (!empty($e->getId())) {
         $obj->error = false;
         $obj->msg = json_encode($e->delete());
