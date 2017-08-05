ecbf94b48b5c1f7265e525df2db53e0a035f07b9
daniel <daniel@daniel-DC1A-S>
2017-08-05 17:56:29 -0300

    Making regular users not see sombere else queue edition

--- view/queue.json.php
+++ view/queue.json.php
@@ -4,10 +4,12 @@ require_once '../objects/Encoder.php';
 require_once '../objects/Login.php';
 header('Content-Type: application/json');
 $rows = Encoder::getAll();
+$totalSkiped = 0;
 foreach ($rows as $key=>$value) {
     if(!Login::isAdmin()){
         if(Login::getStreamerId() != $rows[$key]['streamers_id']){
             unset($rows[$key]);
+            $totalSkiped++;
             continue;
         }
     }    
@@ -16,4 +18,4 @@ foreach ($rows as $key=>$value) {
 }
 $total = Encoder::getTotal();
 
-echo '{  "current": '.$_POST['current'].',"rowCount": '.$_POST['rowCount'].', "total": '.$total.', "rows":'. json_encode($rows).'}';
+echo '{  "current": '.$_POST['current'].',"rowCount": '.$_POST['rowCount'].', "total": '.($total-$totalSkiped).', "rows":'. json_encode($rows).'}';
