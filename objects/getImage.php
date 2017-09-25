48ce80fcaf508d6732cc3bb2727c7a207b6a59df
daniel <daniel@daniel-DC1A-S>
2017-09-25 16:14:33 -0300

    Get image from streaming

--- objects/getImage.php
+++ objects/getImage.php
@@ -4,8 +4,10 @@ $destinationFile = md5($url);
 $destination = sys_get_temp_dir().DIRECTORY_SEPARATOR.$destinationFile.".png";
 if(!file_exists($destination)){
     $exec = "ffmpeg -i {$url} -f image2 -vframes 1 {$destination}";
+    error_log("Exec get Image: {$exec}");
     shell_exec($exec);
 }
 header('Content-Type: image/x-png');
-readfile($destination);
+error_log("Destination get Image: {$destination}");
+echo file_get_contents($destination);
 die();
