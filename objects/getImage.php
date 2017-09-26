76b9099e8f77adc0322e5e29576c338273603d50
daniel <daniel@daniel-DC1A-S>
2017-09-26 09:27:11 -0300

    Get image from streaming

--- objects/getImage.php
+++ objects/getImage.php
@@ -1,16 +1,31 @@
 <?php
 $url = base64_decode($_GET['base64Url']);
 $destinationFile = md5($url);
-$destination = sys_get_temp_dir().DIRECTORY_SEPARATOR.$destinationFile.".png";
-
+$destination = sys_get_temp_dir().DIRECTORY_SEPARATOR.$destinationFile;
 $cache_life = '60'; //caching time, in seconds
+
+if($_GET['format'] === 'png'){
+    header('Content-Type: image/x-png');
+    $destination .= ".".$_GET['format'];
+    $exec = "ffmpeg -i {$url} -f image2 -vframes 1 -y {$destination}";
+}else if($_GET['format'] === 'gif'){
+    header('Content-Type: image/gif');
+    $destination .= ".".$_GET['format'];    
+    //Generate a palette:
+    $ffmpeg ="ffmpeg -y -t 3 -i {$url} -vf fps=10,scale=320:-1:flags=lanczos,palettegen {$destination}palette.png";
+    exec($ffmpeg . " < /dev/null 2>&1", $output, $return_val);
+    $exec ="ffmpeg -ss {$duration} -t {$howLong} -i {$destination} -i {$destination}palette.png -filter_complex \"fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse\" {$destination}";
+}else{
+    error_log("ERROR Destination get Image {$_GET['format']} not suported");
+    die();
+}
+
+
 $filemtime = @filemtime($destination);  // returns FALSE if file does not exist
 if(!$filemtime || (time() - $filemtime >= $cache_life) || !empty($_GET['renew'])){
-    $exec = "ffmpeg -i {$url} -f image2 -vframes 1 -y {$destination}";
     error_log("Exec get Image: {$exec}");
     shell_exec($exec);
 }
-header('Content-Type: image/x-png');
-error_log("Destination get Image: {$destination}");
+error_log("Destination get Image {$_GET['format']}: {$destination}");
 echo file_get_contents($destination);
 die();
