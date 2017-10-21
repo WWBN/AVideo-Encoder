6530338baf1ddd9ff44e14ecdcfe717f4eebea66
daniel <daniel@daniel-DC1A-S>
2017-10-21 10:43:51 -0300

    make get image faster

--- objects/getImage.php
+++ objects/getImage.php
@@ -1,10 +1,19 @@
 <?php
+function fileOlderThen($file, $ageInSeconds){
+    $filemtime = @filemtime($file);  // returns FALSE if file does not exist
+    if(!$filemtime || (time() - $filemtime >= $ageInSeconds)){
+        return true;
+    }
+    return false;
+}
+
 require_once dirname(__FILE__) . '/../videos/configuration.php';
 header('Access-Control-Allow-Origin: *');
 $url = base64_decode($_GET['base64Url']);
 $destinationFile = md5($url);
 $destination = sys_get_temp_dir().DIRECTORY_SEPARATOR.$destinationFile;
-$cache_life = '120'; //caching time, in seconds
+$destinationPallet = "{$destination}palette.png";
+$cache_life = '600'; //caching time, in seconds
 $ob_flush = false;
 
 if($_GET['format'] === 'png'){
@@ -21,32 +30,32 @@ if($_GET['format'] === 'png'){
     header('Content-Type: image/gif');
     $destination .= ".".$_GET['format'];    
     //Generate a palette:
-    $ffmpegPallet ="ffmpeg -y -t 3 -i {$url} -vf fps=10,scale=320:-1:flags=lanczos,palettegen {$destination}palette.png";
+    $ffmpegPallet ="ffmpeg -y -t 3 -i {$url} -vf fps=10,scale=320:-1:flags=lanczos,palettegen {$destinationPallet}";
     $exec ="ffmpeg -y -t 3 -i {$url} -i {$destination}palette.png -filter_complex \"fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse\" {$destination}";
     $destinationTmpFile = "{$global['systemRootPath']}view/img/notfound.gif";
 }else{
     error_log("ERROR Destination get Image {$_GET['format']} not suported");
     die();
 }
+
 if(!is_readable($destination)){
-    $destination = $destinationTmpFile;
+    echo file_get_contents($destinationTmpFile);
+    error_log("Destination get Temp Image {$_GET['format']}: {$destinationTmpFile}");
+}else{
+    // flush old image then encode
+    echo file_get_contents($destination);
+    error_log("Destination get Image {$_GET['format']}: {$destination}");
 }
-// flush old image then encode
-echo file_get_contents($destination);
 ob_flush();
 
-$filemtime = @filemtime($destination);  // returns FALSE if file does not exist
-if(!$filemtime || (time() - $filemtime >= $cache_life) || !empty($_GET['renew'])){
-    if($destination !== $destinationTmpFile && (time() - $filemtime >= ($cache_life*3))){
-        unlink($destination) ;
+if(!file_exists($destination) || fileOlderThen($destination, $cache_life) || !empty($_GET['renew'])){
+    if(!empty($ffmpegPallet) && (!file_exists($destinationPallet) || is_readable($destinationPallet))){
+        $cmd = "{$ffmpegPallet} &> /dev/null && {$exec} &> /dev/null &";
+    }else{
+        $cmd = "{$exec} &> /dev/null &";
     }
-    if(!empty($ffmpegPallet)){
-        exec($ffmpegPallet."   &> /dev/null &");
-        error_log("Exec get Image palette: {$ffmpegPallet}");
-    }
+    exec($cmd);
     error_log("Exec get Image: {$exec}");
-    exec($exec."   &> /dev/null &");
 }
-error_log("Destination get Image {$_GET['format']}: {$destination}");
 
 die();
