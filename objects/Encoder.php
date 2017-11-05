9e26933803b2a4802c7a71b75353656a1086453f
daniel <daniel@daniel-DC1A-S>
2017-11-05 20:47:49 -0300

    Fix get time

--- objects/Encoder.php
+++ objects/Encoder.php
@@ -764,7 +764,7 @@ class Encoder extends Object {
         if ($seconds < 10) {
             $seconds = "0{$seconds}";
         }
-        $minutes = floor($int / 60);
+        $minutes = floor(($int / 60)%60);
         if ($minutes < 10) {
             $minutes = "0{$minutes}";
         }
