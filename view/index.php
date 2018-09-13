0790e36284b8023a76c1d0662b6b9c08da9774f7
daniel <daniel@daniel-DC1A-S>
2018-09-13 14:32:50 -0300

    Fix wrong URL

--- view/index.php
+++ view/index.php
@@ -196,7 +196,9 @@ if (!empty($_GET['noNavbar'])) {
                                         modal.hidePleaseWait();
                                         swal("Sorry!", "Your user or password is wrong!", "error");
                                     } else {
-                                        document.location = document.location+"&justLogin=1";
+                                        var url = new URL(document.location);
+                                        url.searchParams.append('justLogin', 1);
+                                        document.location = url;
                                     }
                                 }
                             });
