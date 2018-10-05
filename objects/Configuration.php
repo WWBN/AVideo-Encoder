77c049aede36f71d083e3a479cfaf46757253559
daniel <daniel@daniel-DC1A-S>
2018-10-05 18:17:06 -0300

    fix strange error

--- objects/Configuration.php
+++ objects/Configuration.php
@@ -1,72 +1,74 @@
 <?php
-if(class_exists('Configuration')){
+
+if (class_exists('Configuration')) {
     return;
-}
-class Configuration extends ObjectYPT {
-    
-    protected $allowedStreamersURL, $defaultPriority, $version, $autodelete; 
-    
-    static function getSearchFieldsNames() {
-        return array('allowedStreamersURL');
-    }
+} else {
 
-    static function getTableName() {
-        return 'configurations';
-    }
+    class Configuration extends ObjectYPT {
 
-    function __construct() {
-        $this->load(1);
-    }
-    
-    function getAllowedStreamersURL() {
-        return $this->allowedStreamersURL;
-    }
+        protected $allowedStreamersURL, $defaultPriority, $version, $autodelete;
 
-    function getDefaultPriority() {
-        return $this->defaultPriority;
-    }
-
-    function setAllowedStreamersURL($allowedStreamersURL) {
-        $this->allowedStreamersURL = $allowedStreamersURL;
-    }
-
-    function setDefaultPriority($defaultPriority) {
-        $this->defaultPriority = $defaultPriority;
-    }
-
-    function getVersion() {
-        return $this->version;
-    }
-
-    function setVersion($version) {
-        $this->version = $version;
-    }
-    
-    function getAutodelete() {
-        return $this->autodelete;
-    }
-
-    function setAutodelete($autodelete) {
-        if(empty($autodelete) || strtolower($autodelete) === 'false'){
-            $autodelete = 0;
-        }else{
-            $autodelete = 1;
+        static function getSearchFieldsNames() {
+            return array('allowedStreamersURL');
         }
-        $this->autodelete = $autodelete;
-    }
-        
-    function currentVersionLowerThen($version) {
-        return version_compare($version, $this->getVersion()) > 0;
-    }
 
-    function currentVersionGreaterThen($version) {
-        return version_compare($version, $this->getVersion()) < 0;
-    }
+        static function getTableName() {
+            return 'configurations';
+        }
 
-    function currentVersionEqual($version) {
-        return version_compare($version, $this->getVersion()) == 0;
-    }
+        function __construct() {
+            $this->load(1);
+        }
 
+        function getAllowedStreamersURL() {
+            return $this->allowedStreamersURL;
+        }
+
+        function getDefaultPriority() {
+            return $this->defaultPriority;
+        }
+
+        function setAllowedStreamersURL($allowedStreamersURL) {
+            $this->allowedStreamersURL = $allowedStreamersURL;
+        }
+
+        function setDefaultPriority($defaultPriority) {
+            $this->defaultPriority = $defaultPriority;
+        }
+
+        function getVersion() {
+            return $this->version;
+        }
+
+        function setVersion($version) {
+            $this->version = $version;
+        }
+
+        function getAutodelete() {
+            return $this->autodelete;
+        }
+
+        function setAutodelete($autodelete) {
+            if (empty($autodelete) || strtolower($autodelete) === 'false') {
+                $autodelete = 0;
+            } else {
+                $autodelete = 1;
+            }
+            $this->autodelete = $autodelete;
+        }
+
+        function currentVersionLowerThen($version) {
+            return version_compare($version, $this->getVersion()) > 0;
+        }
+
+        function currentVersionGreaterThen($version) {
+            return version_compare($version, $this->getVersion()) < 0;
+        }
+
+        function currentVersionEqual($version) {
+            return version_compare($version, $this->getVersion()) == 0;
+        }
+
+    }
 
 }
-    
