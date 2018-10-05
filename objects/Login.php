a3b62d224d5899bc793151f0172f5778104d745f
daniel <daniel@daniel-DC1A-S>
2018-10-05 18:22:11 -0300

    fix strange error

--- objects/Login.php
+++ objects/Login.php
@@ -1,118 +1,121 @@
 <?php
 
-require_once dirname(__FILE__) . '/../videos/configuration.php';
-require_once dirname(__FILE__) . '/Streamer.php';
+if (!class_exists('Login')) {
+    require_once dirname(__FILE__) . '/../videos/configuration.php';
+    require_once dirname(__FILE__) . '/Streamer.php';
 
-class Login {
+    class Login {
 
-    static function run($user, $pass, $youPHPTubeURL, $encodedPass = false) {
-        global $global;
-        if (substr($youPHPTubeURL, -1) !== '/') {
-            $youPHPTubeURL .= "/";
-        }
+        static function run($user, $pass, $youPHPTubeURL, $encodedPass = false) {
+            global $global;
+            if (substr($youPHPTubeURL, -1) !== '/') {
+                $youPHPTubeURL .= "/";
+            }
 
-        $postdata = http_build_query(
-                array(
-                    'user' => $user,
-                    'pass' => $pass,
-                    'encodedPass' => $encodedPass
+            $postdata = http_build_query(
+                    array(
+                        'user' => $user,
+                        'pass' => $pass,
+                        'encodedPass' => $encodedPass
+                    )
+            );
+
+            $opts = array(
+                "ssl" => array(
+                    "verify_peer" => false,
+                    "verify_peer_name" => false,
+                    "allow_self_signed" => true
+                ),
+                'http' => array(
+                    'method' => 'POST',
+                    'header' => 'Content-type: application/x-www-form-urlencoded',
+                    'content' => $postdata
                 )
-        );
+            );
 
-        $opts = array(
-            "ssl" => array(
-                "verify_peer" => false,
-                "verify_peer_name" => false,
-                "allow_self_signed" => true 
-            ),
-            'http' => array(
-                'method' => 'POST',
-                'header' => 'Content-type: application/x-www-form-urlencoded',
-                'content' => $postdata
-            )
-        );
-
-        $context = stream_context_create($opts);
-        $url = $youPHPTubeURL . 'login';
-        $result = url_get_contents($url, $context);
-        if(empty($result)){
-            error_log("Get Login fail, try again");
-            $result = file_get_contents($url, false, $context);
-        }
-        
-        if (empty($result)) {
-            $object = new stdClass();
-            $object->streamer = false;
-            $object->streamers_id = 0;
-            $object->isLogged = false;
-            $object->isAdmin = false;
-            $object->canUpload = false;
-            $object->canComment = false;
-            $object->categories = array();
-            error_log("Error on Login context");
-            error_log($url);
-            error_log($result);
-        } else {
-            $object = json_decode($result);
-            $object->streamer = $youPHPTubeURL;
-            $object->streamers_id = 0;
-            if (!empty($object->canUpload)) {
-                $object->streamers_id = Streamer::createIfNotExists($user, $pass, $youPHPTubeURL, $encodedPass);
+            $context = stream_context_create($opts);
+            $url = $youPHPTubeURL . 'login';
+            $result = url_get_contents($url, $context);
+            if (empty($result)) {
+                error_log("Get Login fail, try again");
+                $result = file_get_contents($url, false, $context);
             }
-            if ($object->streamers_id) {
-                $s = new Streamer($object->streamers_id);
-                $resultV = $s->verify();
-                if(!empty($resultV) && !$resultV->verified){
-                    error_log("Error on Login not verified");
-                    return false;
+
+            if (empty($result)) {
+                $object = new stdClass();
+                $object->streamer = false;
+                $object->streamers_id = 0;
+                $object->isLogged = false;
+                $object->isAdmin = false;
+                $object->canUpload = false;
+                $object->canComment = false;
+                $object->categories = array();
+                error_log("Error on Login context");
+                error_log($url);
+                error_log($result);
+            } else {
+                $object = json_decode($result);
+                $object->streamer = $youPHPTubeURL;
+                $object->streamers_id = 0;
+                if (!empty($object->canUpload)) {
+                    $object->streamers_id = Streamer::createIfNotExists($user, $pass, $youPHPTubeURL, $encodedPass);
                 }
-                
-                $object->isAdmin = $s->getIsAdmin();
-                if (!$encodedPass || $encodedPass === 'false') {
-                    $pass = md5($pass);
+                if ($object->streamers_id) {
+                    $s = new Streamer($object->streamers_id);
+                    $resultV = $s->verify();
+                    if (!empty($resultV) && !$resultV->verified) {
+                        error_log("Error on Login not verified");
+                        return false;
+                    }
+
+                    $object->isAdmin = $s->getIsAdmin();
+                    if (!$encodedPass || $encodedPass === 'false') {
+                        $pass = md5($pass);
+                    }
+                    // update pass
+                    $s->setPass($pass);
+                    $s->save();
                 }
-                // update pass
-                $s->setPass($pass);
-                $s->save();
             }
+            $object->youPHPTubeURL = $url;
+            $object->result = $result;
+            $_SESSION['login'] = $object;
         }
-        $object->youPHPTubeURL = $url;
-        $object->result = $result;
-        $_SESSION['login'] = $object;
-    }
 
-    static function logoff() {
-        unset($_SESSION['login']);
-    }
-
-    static function isLogged() {
-        return !empty($_SESSION['login']->isLogged);
-    }
-
-    static function isAdmin() {
-        return !empty($_SESSION['login']->isAdmin);
-    }
-
-    static function canUpload() {
-        return !empty($_SESSION['login']->canUpload);
-    }
-
-    static function canComment() {
-        return !empty($_SESSION['login']->canComment);
-    }
-
-    static function getStreamerURL() {
-        if (!static::isLogged()) {
-            return false;
+        static function logoff() {
+            unset($_SESSION['login']);
         }
-        return $_SESSION['login']->streamer;
-    }
 
-    static function getStreamerId() {
-        if (!static::isLogged()) {
-            return false;
+        static function isLogged() {
+            return !empty($_SESSION['login']->isLogged);
         }
-        return $_SESSION['login']->streamers_id;
+
+        static function isAdmin() {
+            return !empty($_SESSION['login']->isAdmin);
+        }
+
+        static function canUpload() {
+            return !empty($_SESSION['login']->canUpload);
+        }
+
+        static function canComment() {
+            return !empty($_SESSION['login']->canComment);
+        }
+
+        static function getStreamerURL() {
+            if (!static::isLogged()) {
+                return false;
+            }
+            return $_SESSION['login']->streamer;
+        }
+
+        static function getStreamerId() {
+            if (!static::isLogged()) {
+                return false;
+            }
+            return $_SESSION['login']->streamers_id;
+        }
+
     }
 
 }
--- objects/Streamer.php
+++ objects/Streamer.php
@@ -1,194 +1,197 @@
 <?php
 
-require_once dirname(__FILE__) . '/Configuration.php';
+if (!class_exists('Streamer')) {
+    require_once dirname(__FILE__) . '/Configuration.php';
 
-class Streamer extends ObjectYPT {
+    class Streamer extends ObjectYPT {
 
-    protected $id, $siteURL, $user, $pass, $priority, $isAdmin, $created, $modified;
+        protected $id, $siteURL, $user, $pass, $priority, $isAdmin, $created, $modified;
 
-    static function getSearchFieldsNames() {
-        return array('siteURL');
-    }
-
-    static function getTableName() {
-        return 'streamers';
-    }
-
-    private static function get($user, $siteURL) {
-        global $global;
-        $sql = "SELECT * FROM  " . static::getTableName() . " WHERE user = '{$user}' AND lower(siteURL) = lower('{$siteURL}') LIMIT 1";
-        //echo $sql;exit;
-        $res = $global['mysqli']->query($sql);
-        if ($res) {
-            return $res->fetch_assoc();
-        } else {
-            die($sql . '\nError : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
+        static function getSearchFieldsNames() {
+            return array('siteURL');
         }
-        return false;
-    }
 
-    private static function getFirst() {
-        global $global;
-        $sql = "SELECT * FROM  " . static::getTableName() . " LIMIT 1";
-
-        $res = $global['mysqli']->query($sql);
-        if ($res) {
-            return $res->fetch_assoc();
-        } else {
-            die($sql . '\nError : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
+        static function getTableName() {
+            return 'streamers';
         }
-        return false;
-    }
 
-    static function getFirstURL() {
-        $row = static::getFirst();
-        return $row['siteURL'];
-    }
-
-    static function createIfNotExists($user, $pass, $siteURL, $encodedPass = false) {
-        if (!$encodedPass || $encodedPass === 'false') {
-            $pass = md5($pass);
-        }
-        if (substr($siteURL, -1) !== '/') {
-            $siteURL .= "/";
-        }
-        if ($row = static::get($user, $siteURL)) {
-            if (!empty($row['id'])) {
-                return $row['id'];
+        private static function get($user, $siteURL) {
+            global $global;
+            $sql = "SELECT * FROM  " . static::getTableName() . " WHERE user = '{$user}' AND lower(siteURL) = lower('{$siteURL}') LIMIT 1";
+            //echo $sql;exit;
+            $res = $global['mysqli']->query($sql);
+            if ($res) {
+                return $res->fetch_assoc();
+            } else {
+                die($sql . '\nError : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
             }
-        }
-
-        if (static::isURLAllowed($siteURL)) {
-            $config = new Configuration();
-            $s = new Streamer('');
-            $s->setUser($user);
-            $s->setPass($pass);
-            $s->setSiteURL($siteURL);
-            $s->setIsAdmin(0);
-            $s->setPriority($config->getDefaultPriority());
-            return $s->save();
-        } else {
             return false;
         }
-    }
-    
-    function verify(){
-        $url = $this->getSiteURL();
-        $cacheFile = sys_get_temp_dir()."/".md5($url)."_verify.log";
-        $lifetime = 3600;//1 hour
-        error_log("Verification Start {$url}");
-        $verifyURL = "";
-        if(!file_exists($cacheFile) || (time() > (filemtime($cacheFile)+$lifetime))){
-            error_log("Verification Creating the Cache {$url}");
-            $verifyURL = "https://search.youphptube.com/verify.php?url=". urlencode($url);
-            $result = url_get_contents($verifyURL);
-            file_put_contents($cacheFile, $result);
-        }else{
-            error_log("Verification GetFrom Cache {$url}");
-            $result = file_get_contents($cacheFile);
-        }
-        error_log("Verification Response ($verifyURL): {$result}");
-        return json_decode($result);
-    }
 
-    static function isURLAllowed($siteURL) {
-        if (substr($siteURL, -1) !== '/') {
-            $siteURL .= "/";
+        private static function getFirst() {
+            global $global;
+            $sql = "SELECT * FROM  " . static::getTableName() . " LIMIT 1";
+
+            $res = $global['mysqli']->query($sql);
+            if ($res) {
+                return $res->fetch_assoc();
+            } else {
+                die($sql . '\nError : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
+            }
+            return false;
         }
-        $config = new Configuration();
-        $urls = $config->getAllowedStreamersURL();
-        if (empty($urls)) {
-            return true;
+
+        static function getFirstURL() {
+            $row = static::getFirst();
+            return $row['siteURL'];
         }
-        $allowed = explode(PHP_EOL, $urls);
-        $return = false;
-        if (empty($allowed)) {
-            $return = true;
-        } else {
-            foreach ($allowed as $value) {
-                if (empty($value)) {
-                    continue;
-                }
-                $value = trim($value);
-                if (substr($value, -1) !== '/') {
-                    $value .= "/";
-                }
-                //var_dump($siteURL,$value);
-                error_log("$siteURL == $value");
-                if ($siteURL == $value) {
-                    $return = true;
-                    break;
+
+        static function createIfNotExists($user, $pass, $siteURL, $encodedPass = false) {
+            if (!$encodedPass || $encodedPass === 'false') {
+                $pass = md5($pass);
+            }
+            if (substr($siteURL, -1) !== '/') {
+                $siteURL .= "/";
+            }
+            if ($row = static::get($user, $siteURL)) {
+                if (!empty($row['id'])) {
+                    return $row['id'];
                 }
             }
+
+            if (static::isURLAllowed($siteURL)) {
+                $config = new Configuration();
+                $s = new Streamer('');
+                $s->setUser($user);
+                $s->setPass($pass);
+                $s->setSiteURL($siteURL);
+                $s->setIsAdmin(0);
+                $s->setPriority($config->getDefaultPriority());
+                return $s->save();
+            } else {
+                return false;
+            }
         }
-        return $return;
-    }
 
-    function getId() {
-        return $this->id;
-    }
-
-    function getSiteURL() {
-        return $this->siteURL;
-    }
-
-    function getUser() {
-        return $this->user;
-    }
-
-    function getPass() {
-        return $this->pass;
-    }
-
-    function getCreated() {
-        return $this->created;
-    }
-
-    function getModified() {
-        return $this->modified;
-    }
-
-    function setId($id) {
-        $this->id = $id;
-    }
-
-    function setSiteURL($siteURL) {
-        if (!empty($siteURL) && substr($siteURL, -1) !== '/') {
-            $siteURL .= "/";
+        function verify() {
+            $url = $this->getSiteURL();
+            $cacheFile = sys_get_temp_dir() . "/" . md5($url) . "_verify.log";
+            $lifetime = 3600; //1 hour
+            error_log("Verification Start {$url}");
+            $verifyURL = "";
+            if (!file_exists($cacheFile) || (time() > (filemtime($cacheFile) + $lifetime))) {
+                error_log("Verification Creating the Cache {$url}");
+                $verifyURL = "https://search.youphptube.com/verify.php?url=" . urlencode($url);
+                $result = url_get_contents($verifyURL);
+                file_put_contents($cacheFile, $result);
+            } else {
+                error_log("Verification GetFrom Cache {$url}");
+                $result = file_get_contents($cacheFile);
+            }
+            error_log("Verification Response ($verifyURL): {$result}");
+            return json_decode($result);
         }
-        $this->siteURL = $siteURL;
-    }
 
-    function setUser($user) {
-        $this->user = $user;
-    }
+        static function isURLAllowed($siteURL) {
+            if (substr($siteURL, -1) !== '/') {
+                $siteURL .= "/";
+            }
+            $config = new Configuration();
+            $urls = $config->getAllowedStreamersURL();
+            if (empty($urls)) {
+                return true;
+            }
+            $allowed = explode(PHP_EOL, $urls);
+            $return = false;
+            if (empty($allowed)) {
+                $return = true;
+            } else {
+                foreach ($allowed as $value) {
+                    if (empty($value)) {
+                        continue;
+                    }
+                    $value = trim($value);
+                    if (substr($value, -1) !== '/') {
+                        $value .= "/";
+                    }
+                    //var_dump($siteURL,$value);
+                    error_log("$siteURL == $value");
+                    if ($siteURL == $value) {
+                        $return = true;
+                        break;
+                    }
+                }
+            }
+            return $return;
+        }
 
-    function setPass($pass) {
-        $this->pass = $pass;
-    }
+        function getId() {
+            return $this->id;
+        }
 
-    function setCreated($created) {
-        $this->created = $created;
-    }
+        function getSiteURL() {
+            return $this->siteURL;
+        }
 
-    function setModified($modified) {
-        $this->modified = $modified;
-    }
+        function getUser() {
+            return $this->user;
+        }
 
-    function getPriority() {
-        return $this->priority;
-    }
+        function getPass() {
+            return $this->pass;
+        }
 
-    function setPriority($priority) {
-        $this->priority = $priority;
-    }
+        function getCreated() {
+            return $this->created;
+        }
 
-    function getIsAdmin() {
-        return $this->isAdmin;
-    }
+        function getModified() {
+            return $this->modified;
+        }
 
-    function setIsAdmin($isAdmin) {
-        $this->isAdmin = $isAdmin;
+        function setId($id) {
+            $this->id = $id;
+        }
+
+        function setSiteURL($siteURL) {
+            if (!empty($siteURL) && substr($siteURL, -1) !== '/') {
+                $siteURL .= "/";
+            }
+            $this->siteURL = $siteURL;
+        }
+
+        function setUser($user) {
+            $this->user = $user;
+        }
+
+        function setPass($pass) {
+            $this->pass = $pass;
+        }
+
+        function setCreated($created) {
+            $this->created = $created;
+        }
+
+        function setModified($modified) {
+            $this->modified = $modified;
+        }
+
+        function getPriority() {
+            return $this->priority;
+        }
+
+        function setPriority($priority) {
+            $this->priority = $priority;
+        }
+
+        function getIsAdmin() {
+            return $this->isAdmin;
+        }
+
+        function setIsAdmin($isAdmin) {
+            $this->isAdmin = $isAdmin;
+        }
+
     }
 
 }
