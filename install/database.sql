b5ea42f3418c38d79896e7879e465fc7984daeac
Scott Yannitell <scott@ohiovr.com>
2018-12-11 04:00:32 -0500

    sync with master

--- install/database.sql
+++ install/database.sql
@@ -92,18 +92,18 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
--- preserve aspect ratio https://stackoverflow.com/questions/32463531/ffmpeg-resize-largest-video-dimension-to-320
+
 INSERT INTO `formats` VALUES 
-(1,'MP4 Low','ffmpeg -i {$pathFileName} -vf \'scale=360:360:force_original_aspect_ratio=decrease, pad=ceil(iw/2)*2:ceil(ih/2)*2\' -movflags faststart -preset ultrafast -vcodec h264 -acodec aac -strict -2 -y {$destinationFile}',now(),now(),'mp4','mp4',10),
-(2,'WEBM Low','ffmpeg -i {$pathFileName} -vf \'scale=360:360:force_original_aspect_ratio=decrease, pad=ceil(iw/2)*2:ceil(ih/2)*2\'  -movflags faststart -preset ultrafast -f webm -c:v libvpx -b:v 1M -acodec libvorbis -y {$destinationFile}',now(),now(),'webm','mp4',20),
+(1,'MP4 Low','ffmpeg -i {$pathFileName} -vf scale=-2:360 -movflags faststart -preset ultrafast -vcodec h264 -acodec aac -strict -2 -y {$destinationFile}',now(),now(),'mp4','mp4',10),
+(2,'WEBM Low','ffmpeg -i {$pathFileName} -vf scale=-2:360 -movflags faststart -preset ultrafast -f webm -c:v libvpx -b:v 1M -acodec libvorbis -y {$destinationFile}',now(),now(),'webm','mp4',20),
 (3,'MP3','ffmpeg -i {$pathFileName} -acodec libmp3lame -y {$destinationFile}',now(),now(),'mp3','mp3',30),
 (4,'OGG','ffmpeg -i {$pathFileName} -acodec libvorbis -y {$destinationFile}',now(),now(),'ogg','mp3',40),
 (5,'MP3 to Spectrum.MP4','ffmpeg -i {$pathFileName} -filter_complex \'[0:a]showwaves=s=640x360:mode=line,format=yuv420p[v]\' -map \'[v]\' -map 0:a -c:v libx264 -c:a copy {$destinationFile}',now(),now(),'mp4','mp3',50),
 (6,'Video.MP4 to Audio.MP3','ffmpeg -i {$pathFileName} -y {$destinationFile}',now(),now(),'mp3','mp4',60),
-(7,'MP4 SD','ffmpeg -i {$pathFileName} -vf \'scale=720:720:force_original_aspect_ratio=decrease, pad=ceil(iw/2)*2:ceil(ih/2)*2\'  -movflags faststart -preset ultrafast -vcodec h264 -acodec aac -strict -2 -y {$destinationFile}',now(),now(),'mp4','mp4',11),
-(8,'MP4 HD','ffmpeg -i {$pathFileName} -vf \'scale=1280:1280:force_original_aspect_ratio=decrease, pad=ceil(iw/2)*2:ceil(ih/2)*2\'  -movflags faststart -preset ultrafast -vcodec h264 -acodec aac -strict -2 -y {$destinationFile}',now(),now(),'mp4','mp4',12),
-(9,'WEBM SD','ffmpeg -i {$pathFileName} -vf \'scale=720:720:force_original_aspect_ratio=decrease, pad=ceil(iw/2)*2:ceil(ih/2)*2\'  -movflags faststart -preset ultrafast -f webm -c:v libvpx -b:v 1M -acodec libvorbis -y {$destinationFile}',now(),now(),'webm','mp4',21),
-(10,'WEBM HD','ffmpeg -i {$pathFileName} -vf \'scale=1280:1280:force_original_aspect_ratio=decrease, pad=ceil(iw/2)*2:ceil(ih/2)*2\'  -movflags faststart -preset ultrafast -f webm -c:v libvpx -b:v 1M -acodec libvorbis -y {$destinationFile}',now(),now(),'webm','mp4',22),
+(7,'MP4 SD','ffmpeg -i {$pathFileName} -vf scale=-2:540 -movflags faststart -preset ultrafast -vcodec h264 -acodec aac -strict -2 -y {$destinationFile}',now(),now(),'mp4','mp4',11),
+(8,'MP4 HD','ffmpeg -i {$pathFileName} -vf scale=-2:720 -movflags faststart -preset ultrafast -vcodec h264 -acodec aac -strict -2 -y {$destinationFile}',now(),now(),'mp4','mp4',12),
+(9,'WEBM SD','ffmpeg -i {$pathFileName} -vf scale=-2:540 -movflags faststart -preset ultrafast -f webm -c:v libvpx -b:v 1M -acodec libvorbis -y {$destinationFile}',now(),now(),'webm','mp4',21),
+(10,'WEBM HD','ffmpeg -i {$pathFileName} -vf scale=-2:720 -movflags faststart -preset ultrafast -f webm -c:v libvpx -b:v 1M -acodec libvorbis -y {$destinationFile}',now(),now(),'webm','mp4',22),
 (11,'Video to Spectrum','60-50-10',now(),now(),'mp4','mp4',70),
 (12,'Video to Audio','60-40',now(),now(),'mp3','mp4',71),
 (13,'Both Video','10-20',now(),now(),'mp4','mp4',72),
