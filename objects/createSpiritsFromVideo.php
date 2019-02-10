b36821e84a3231e6ba3bb4a074ce1409d2d59b53
daniel <daniel@daniel-DC1A-S>
2019-02-10 13:46:09 -0300

    Improve Create spirits

---
+++ objects/createSpiritsFromVideo.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once dirname(__FILE__) . '/../videos/configuration.php';
+header('Access-Control-Allow-Origin: *');
+
+$pxBetweenTiles = 0;
+
+$url = $argv[1];
+$step = $argv[2];
+$tileWidth = $argv[3];
+$tileHeight = $argv[4];
+$imageFileName = $argv[5];
+$numberOfTiles = $argv[6];
+$baseName = $argv[7];
+
+$dirname = $global['systemRootPath'] . "videos/thumbs_{$baseName}/";
+if (is_dir($dirname)) {
+    if (time() - filemtime($dirname) > 2 * 3600) {
+        // file older than 2 hours
+        error_log("CreateSpirits:  request is older then 2 hours, we will try again");
+    } else {
+        error_log("CreateSpirits:  we are working on it, please wait");
+        exit;
+    }
+}
+
+error_log("CreateSpirits:  creating directory {$dirname}");
+mkdir($dirname);
+
+$width = $tileWidth + $pxBetweenTiles;
+$height = $tileHeight + $pxBetweenTiles;
+$mapWidth = ($tileWidth + $pxBetweenTiles) * 10;
+$mapHeight = $tileHeight * (ceil($numberOfTiles / 10));
+
+$cmd = "ffmpeg -i \"{$url}\" -vf fps=1/{$step} -s {$tileWidth}x{$tileHeight} {$dirname}out%03d.png";
+error_log("CreateSpirits: $cmd");
+//var_dump($duration, $videoLength);echo $cmd;exit;
+exec($cmd);
+
+$images = glob($dirname . "*.png");
+$srcImagePaths = Array();
+
+foreach ($images as $image) {
+    $srcImagePaths[] = $image;
+}
+//var_dump($dirname, $images, $srcImagePaths);exit;
+//https://stackoverflow.com/questions/30429383/combine-16-images-into-1-big-image-with-php
+
+$mapImage = imagecreatetruecolor($mapWidth, $mapHeight);
+$bgColor = imagecolorallocate($mapImage, 50, 40, 0);
+imagefill($mapImage, 0, 0, $bgColor);
+
+/* * COPY SOURCE IMAGES TO MAP */
+foreach ($srcImagePaths as $index => $srcImagePath) {
+    $tileImg = imagecreatefrompng($srcImagePath);
+    imagecopy($mapImage, $tileImg, $width * ($index % 10), $height * (floor($index / 10)), 0, 0, $tileWidth, $tileHeight);
+    imagedestroy($tileImg);
+}
+
+/* * SAVE THUMBNAIL IMAGE */
+imagejpeg($mapImage, $imageFileName); //change argument to $mapImage to output the original size image
+
+
+$it = new RecursiveDirectoryIterator($dirname, RecursiveDirectoryIterator::SKIP_DOTS);
+$files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+foreach ($files as $file) {
+    if ($file->isDir()) {
+        rmdir($file->getRealPath());
+    } else {
+        unlink($file->getRealPath());
+    }
+}
+
+error_log("CreateSpirits:  removing directory {$dirname}");
+rmdir($dirname);
+/* * 
+ */
--- objects/getSpiritsFromVideo.php
+++ objects/getSpiritsFromVideo.php
@@ -4,93 +4,42 @@ require_once dirname(__FILE__) . '/../videos/configuration.php';
 require_once '../objects/Encoder.php';
 header('Access-Control-Allow-Origin: *');
 
-//https://stackoverflow.com/questions/30429383/combine-16-images-into-1-big-image-with-php
-
-function tempdir() {
-    global $global;
-    $tempfile= tempnam($global['systemRootPath'],'');
-    // you might want to reconsider this line when using this snippet.
-    // it "could" clash with an existing directory and this line will
-    // try to delete the existing one. Handle with caution.
-    if (file_exists($tempfile)) { unlink($tempfile); }
-    mkdir($tempfile);
-    if (is_dir($tempfile)) { return $tempfile; }
-}
-
-if(empty($_GET['totalClips'])){
+if (empty($_GET['totalClips'])) {
     $_GET['totalClips'] = 100;
 }
 
 $url = base64_decode($_GET['base64Url']);
-
+$baseName = md5($_GET['base64Url']) ;
+$imageFileName = $global['systemRootPath'] . "videos/sprit_{$baseName}.jpg";
 //$url = "http://127.0.0.1/YouPHPTube/videos/_YPTuniqid_5a01ef79b04ec6.24051213_HD.mp4";
 
 $tileWidth = $_GET['tileWidth'];
 $numberOfTiles = $_GET['totalClips'];
 
-$tileHeight = intval($tileWidth/16*9);
-$pxBetweenTiles = 0;
+$tileHeight = intval($tileWidth / 16 * 9);
 
-if(!empty($_GET['duration'])){
+if (!empty($_GET['duration'])) {
     $duration = $_GET['duration'];
-}else{
+} else {
     $duration = Encoder::getDurationFromFile($url);
 }
 
 $videoLength = parseDurationToSeconds($duration);
 
-
-$width = $tileWidth+$pxBetweenTiles;
-$height = $tileHeight+$pxBetweenTiles;
 $step = $videoLength / $numberOfTiles;
-$mapWidth = ($tileWidth + $pxBetweenTiles) * 10;
-$mapHeight = $tileHeight * (ceil($numberOfTiles/10));
 
-$id = uniqid();
-$dirname = $global['systemRootPath']."videos/tail{$id}/";
-
-mkdir($dirname);
-
-$cmd = "ffmpeg -i \"{$url}\" -vf fps=1/{$step}  -s {$tileWidth}x{$tileHeight} {$dirname}out%03d.png";
-
-//var_dump($duration, $videoLength);echo $cmd;exit;
-exec($cmd);
-
-$images = glob($dirname . "*.png");
-$srcImagePaths = Array();
-
-foreach ($images as $image) {
-    $srcImagePaths[] = $image;
-}
-//var_dump($dirname, $images, $srcImagePaths);exit;
-//https://stackoverflow.com/questions/30429383/combine-16-images-into-1-big-image-with-php
-
-$mapImage = imagecreatetruecolor($mapWidth, $mapHeight);
-$bgColor = imagecolorallocate($mapImage, 50, 40, 0);
-imagefill($mapImage, 0, 0, $bgColor);
-
-/* * COPY SOURCE IMAGES TO MAP */
-foreach ($srcImagePaths as $index => $srcImagePath) {
-    $tileImg = imagecreatefrompng($srcImagePath);
-    imagecopy($mapImage, $tileImg, $width*($index%10), $height*(floor($index/10)), 0, 0, $tileWidth, $tileHeight);
-    imagedestroy($tileImg);
-}
-
-/* * OUTPUT THUMBNAIL IMAGE */
 header("Content-type: image/jpeg");
-imagejpeg($mapImage); //change argument to $mapImage to output the original size image
-
-
-$it = new RecursiveDirectoryIterator($dirname, RecursiveDirectoryIterator::SKIP_DOTS);
-$files = new RecursiveIteratorIterator($it,
-             RecursiveIteratorIterator::CHILD_FIRST);
-foreach($files as $file) {
-    if ($file->isDir()){
-        rmdir($file->getRealPath());
-    } else {
-        unlink($file->getRealPath());
+if (!file_exists($imageFileName)) {
+    // display a dummy image
+    echo file_get_contents($global['systemRootPath'] . "view/img/creatingImages.jpg");
+    //call createsprits
+    $command = ("php '{$global['systemRootPath']}objects/createSpiritsFromVideo.php' '$url' '$step' '$tileWidth' '$tileHeight' '$imageFileName' '$numberOfTiles' '$baseName'");
+    error_log("getSpritsFromVideo: {$command}");
+    exec($command . " < /dev/null 2>&1", $output, $return_val);
+    if ($return_val !== 0) {
+        error_log("getSpritsFromVideo ERROR: ". json_encode($output)." {$command} ");
     }
+} else {
+    echo file_get_contents($imageFileName);
+    unlink($imageFileName);
 }
-rmdir($dirname);
-/* * 
- */
