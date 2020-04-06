3e0fa55125ecf87cbadfd1caf252ccf1c284005f
daniel <daniel@daniel-DC1A-S>
2020-04-06 14:45:30 -0300

    https://github.com/WWBN/AVideo/issues/2823

--- objects/getImage.php
+++ objects/getImage.php
@@ -47,8 +47,8 @@ if($_GET['format'] === 'png'){
     header('Content-Type: image/gif');
     $destination .= ".".$_GET['format'];    
     //Generate a palette:
-    $ffmpegPallet ="ffmpeg -y -t 3 -i {$url} -vf fps=10,scale=320:-1:flags=lanczos,palettegen {$destinationPallet}";
-    $exec ="ffmpeg -y -t 3 -i {$url} -i {$destinationPallet} -filter_complex \"fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse\" {$destination}";
+    $ffmpegPallet ="ffmpeg -y -t 3 -i \"{$url}\" -vf fps=10,scale=320:-1:flags=lanczos,palettegen {$destinationPallet}";
+    $exec ="ffmpeg -y -t 3 -i \"{$url}\" -i {$destinationPallet} -filter_complex \"fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse\" {$destination}";
     $destinationTmpFile = "{$global['systemRootPath']}view/img/notfound.gif";
 }else if($_GET['format'] === 'webp'){
     // gif image has the double lifetime
