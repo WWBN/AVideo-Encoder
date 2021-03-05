0d1141547fee690a676d3f359bc5a95fdf1c7c28
daniel <daniel@daniel-DC1A-S>
2021-03-05 10:22:09 -0300

    Make sure you get the thumbs from youtube

--- objects/getImage.php
+++ objects/getImage.php
@@ -34,8 +34,7 @@ testTime(__LINE__);
 if(preg_match('/(youtube.com|youtu.be|vimeo.com)/', $url)){
     require_once $global['systemRootPath'] . 'objects/Encoder.php';
     header('Content-Type: image/jpg');
-    $destination = Encoder::getThumbsFromLink($url, true);
-    die(file_get_contents($destination));
+    die(Encoder::getThumbsFromLink($url));
 }else
 if($_GET['format'] === 'png'){
     header('Content-Type: image/x-png');
