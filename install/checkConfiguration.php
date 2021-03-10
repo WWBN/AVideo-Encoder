5ebd2e7c6f25cb3f321900520f86ae0046f10230
daniel <daniel@daniel-DC1A-S>
2021-03-10 11:09:03 -0300

    Site admin can now add new Usergroups and categories directly into the encoder The encoder will use the Streamer theme and javascript https://github.com/WWBN/AVideo/issues/4621 Create a new version to make sure they are compatible streamer (v10.5) and the encoder (v3.8)

--- install/checkConfiguration.php
+++ install/checkConfiguration.php
@@ -4,7 +4,7 @@ if (file_exists("../videos/configuration.php")) {
     error_log("Can not create configuration again: ".  json_encode($_SERVER));
     exit;
 }
-$installationVersion = "3.7";
+$installationVersion = "3.8";
 
 header('Content-Type: application/json');
 
---
+++ update/updateDb.v3.8.sql
@@ -0,0 +1,1 @@
+UPDATE configurations SET  version = '3.8', modified = now() WHERE id = 1;
