7c80c1fa013f9cb24f79cc23badd30894d0f0258
daniel <daniel@daniel-DC1A-S>
2021-03-10 22:51:52 -0300

    https://github.com/WWBN/AVideo-Encoder/issues/355#issuecomment-796359698

--- install/index.php
+++ install/index.php
@@ -6,9 +6,10 @@ require_once '../objects/functions.php';
     <head>
         <title>Install AVideo</title>
         <link rel="icon" href="../view/img/favicon.png">
-        <link href="../view/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="../view/js/seetalert/sweetalert.css" rel="stylesheet" type="text/css"/>
-        <script src="../view/js/jquery-3.2.0.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     </head>
 
     <body>
