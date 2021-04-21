b125bbb9b0b77240be696475b9c11a317458471c
daniel <daniel@daniel-DC1A-S>
2021-04-21 10:00:36 -0300

    One more option for command line

---
+++ install/reencodeAllVideos.php
@@ -0,0 +1,26 @@
+<?php
+//streamer config
+require_once '../videos/configuration.php';
+
+if(!isCommandLineInterface()){
+    return die('Command Line only');
+}
+
+if(!empty($userName)){
+    $sql = "UPDATE encoder_queue SET status = 'queue' where id > 0";
+    echo $sql.PHP_EOL;         
+    $insert_row = $global['mysqli']->query($sql);
+            
+    if ($insert_row) {
+        echo "All set to queue".PHP_EOL;
+    } else {
+        die($sql . ' Error : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
+    }
+}
+echo "Bye";
+echo "\n";
+die();
+
+
+
+
