de3caa4e322cebd89e0af0b0f4706f3d66e80223
daniel <daniel@daniel-DC1A-S>
2022-06-28 12:44:42 -0300

    Tool to kill hanging process

--- install/killOldProcess.php
+++ install/killOldProcess.php
@@ -10,8 +10,28 @@ $output = shell_exec($cmd);
 foreach(preg_split("/((\r?\n)|(\r\n?))/", $output) as $line){
     if(strpos($line, '-f image2 -s') !== false){
         echo $line.PHP_EOL;        
+        preg_match('/([0-9]+) +([0-9:]+)/', $line, $matches);
+        $etime = intval(str_replace(':', '', $matches[2]));
+        if($etime>100){ // 1 minute
+            $pid = intval($matches[1]);
+            if(!empty($pid)){
+                $cmd = 'kill -9 '.$pid;
+                echo shell_exec($cmd);
+                continue;
+            }
+        }
     }
     if(strpos($line, '-y -ss 3 -t 3') !== false){
-        echo $line.PHP_EOL;        
+        echo $line.PHP_EOL; 
+        preg_match('/([0-9]+) +([0-9:]+)/', $line, $matches);
+        $etime = intval(str_replace(':', '', $matches[2]));
+        if($etime>100){ // 1 minute
+            $pid = intval($matches[1]);
+            if(!empty($pid)){
+                $cmd = 'kill -9 '.$pid;
+                echo shell_exec($cmd);
+                continue;
+            }
+        }
     }
 } 
