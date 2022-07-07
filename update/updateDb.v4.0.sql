e8ad878ad15c46d51b991a728b8aa067c6070917
daniel <daniel@daniel-DC1A-S>
2022-07-06 21:54:11 -0300

    New Password Security improvements

--- update/updateDb.v4.0.sql
+++ update/updateDb.v4.0.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `streamers` 
 CHANGE COLUMN `user` `user` VARCHAR(255) NOT NULL ,
 CHANGE COLUMN `pass` `pass` VARCHAR(255) NOT NULL ;
-UPDATE configurations SET  version = '4.0', modified = now() WHERE id = 1;
+UPDATE configurations_encoder SET  version = '4.0', modified = now() WHERE id = 1;
