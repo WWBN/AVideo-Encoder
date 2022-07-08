cff3d0d5a4a712ed43e0de9e5f4ecc64a1c6d705
daniel <daniel@daniel-DC1A-S>
2022-07-08 11:50:42 -0300

    https://github.com/WWBN/AVideo-Encoder/issues/444#issuecomment-1179036473

--- objects/Object.php
+++ objects/Object.php
@@ -175,7 +175,8 @@ abstract class ObjectYPT implements ObjectInterface{
             }
             return $id;
         } else {
-            die($sql . ' Error : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
+            error_log($sql . ' Error : (' . $global['mysqli']->errno . ') ' . $global['mysqli']->error);
+            return false;
         }
     }
     
