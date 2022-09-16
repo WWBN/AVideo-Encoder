918631f1f7021fc2e40c4f796a8160a04cbe29b4
daniel <daniel@daniel-DC1A-S>
2022-09-16 09:13:32 -0300

    Add a command line tool to delete all videos from queue

--- install/deleteAllQueue.php
+++ install/deleteAllQueue.php
@@ -6,6 +6,7 @@ require_once '../videos/configuration.php';
 if (!isCommandLineInterface()) {
     return die('Command Line only');
 }
+require_once $global['systemRootPath'].'objects/Encoder.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
