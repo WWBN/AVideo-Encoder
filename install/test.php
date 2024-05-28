4cd6bacd5976fcb8a5a02cb95a4ce68cc0402580
Daniel Neto <me@danielneto.com>
2024-05-28 10:08:29 -0300

    update

--- install/test.php
+++ install/test.php
@@ -7,21 +7,22 @@ if (!isCommandLineInterface()) {
     return die('Command Line only');
 }
 
-$filesURL = array(
-    'http://4k.ypt.me/1080/Christmas_Tree_Bokeh.mp4',
-    'http://4k.ypt.me/1080/Galaxy_With_Customization.mp4',
-    'http://4k.ypt.me/1080/Earth_from_Space.mov',
-    'http://4k.ypt.me/1080/Snow.mp4',
-    'http://4k.ypt.me/4K/Elecard_about_Tomsk_part1_HEVC_UHD.mp4',
-    //'https://www.youtube.com/watch?v=PaXVZJFfgJA', //4K HDR IMAX • Spider-Man fights Mysterio's Illusions ᵈᵗˢ⁻ʰᵈ
-    //'http://4k.ypt.me/4K/Stream1_AV1_4K_8.5mbps.webm'
-);
 
 $_POST['user'] = 'admin';
 $_POST['pass'] = '123';
 //$_POST['inputAutoHLS'] = true;
 $_POST['notifyURL'] = str_replace("Encoder/", "", $global['webSiteRootURL']);
 $_POST['notifyURL'] = str_ireplace(array('rtmp://'), array(''), $_POST['notifyURL']);
+
+/*
+$filesURL = array(
+    'http://4k.ypt.me/1080/Christmas_Tree_Bokeh.mp4',
+    'http://4k.ypt.me/1080/Galaxy_With_Customization.mp4',
+    'http://4k.ypt.me/1080/Earth_from_Space.mov',
+    'http://4k.ypt.me/1080/Snow.mp4',
+    'http://4k.ypt.me/4K/Elecard_about_Tomsk_part1_HEVC_UHD.mp4',
+);
+
 foreach ($filesURL as $value) {
     $_POST['fileURI'] = $value;
     $path_parts = pathinfo($_POST['fileURI']);
@@ -31,6 +32,23 @@ foreach ($filesURL as $value) {
     include $global['systemRootPath'].'view/queue.php';
     echo "Include: {$_POST['filename']}" . PHP_EOL;
 }
+*/
+$filesURL = array(
+    'https://www.youtube.com/watch?v=PaXVZJFfgJA',
+    'https://www.youtube.com/watch?v=WO2b03Zdu4Q',
+    'https://www.youtube.com/watch?v=R5LAPvUKGvc',
+    'https://www.youtube.com/watch?v=njX2bu-_Vw4',
+    'https://www.youtube.com/watch?v=ee9i6oMqShk',
+    'https://www.youtube.com/watch?v=mkggXE5e2yk',
+    //'https://www.youtube.com/watch?v=e4zRaWV2YEQ',
+);
+
+foreach ($filesURL as $value) {
+    $_POST['videoURL'] = $value;
+    echo "Processing: ".json_encode($_POST) . PHP_EOL;
+    include $global['systemRootPath'].'view/youtubeDl.json.php';
+}
+
 
 echo "Bye" . PHP_EOL;
 die();
