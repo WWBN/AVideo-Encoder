b9e45d40e76bcd1a13aa03908aabc58d55ec6d27
daniel <daniel@daniel-DC1A-S>
2024-06-19 20:11:17 -0300

    Update

--- objects/Encoder.php
+++ objects/Encoder.php
@@ -2647,7 +2647,7 @@ class Encoder extends ObjectYPT
         $f = new Format($e->getFormats_id());
         $format = $f->getExtension();
         $response = Encoder::sendFile('', 0, $format, $e);
-        error_log("queue : Encoder::sendFile done line=".__LINE__).' '.json_encode($response);
+        error_log("queue: Encoder::sendFile done line=".__LINE__).' '.json_encode($response);
         //var_dump($response);exit;
         if (!empty($response->response->video_id)) {
             $obj->videos_id = $response->response->video_id;
