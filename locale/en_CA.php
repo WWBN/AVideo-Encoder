e7a389c5de7586c7478dda492efcd10773505405
Caleb Mazalevskis <maikuolan@gmail.com>
2024-07-16 13:17:22 +0800

    Add Canadian English support.

---
+++ locale/en_CA.php
@@ -0,0 +1,5 @@
+<?php
+// Because we're including the US English file here as a default to fall back
+// on, we should only include translations in this file here where they differ
+// from their US counterparts.
+include __DIR__ . '/en_US.php';
