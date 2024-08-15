4afd62813a02257510c81fe3e48193ccaf6165d0
Daniel Neto <me@danielneto.com>
2024-08-15 19:01:43 -0300

    Update

--- view/index.php
+++ view/index.php
@@ -910,7 +910,7 @@ if (empty($_COOKIE['format']) && !empty($_SESSION['format'])) {
                                 }
                                 var filename = row.title;
                                 if (filename.startsWith("original_v_")) {
-                                    filename = '<a href="' + row.streamer + 'video/' + filename + '" target="_blank">' + filename + '</a>';
+                                    filename = '<a href="' + row.streamer + 'videos/' + filename + '" target="_blank">' + filename + '</a>';
                                 }
                                 title += '<br>' + filename;
 
